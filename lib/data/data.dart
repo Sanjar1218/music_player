5483b676921e343fa4e22f0b3da393f38e92c26e
Sanjar17 <sanjarsaidov173@gmail.com>
2023-06-02 11:25:05 +0500

    init

--- lib/data/data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:music_player/models/main_model.dart';
-
-List<Music> musicList = [
-  Music(music_author: "Ozodbek Nazarbekov", music_title: "Music name"),
-  Music(music_author: "Rauf Faik", music_title: "Music name"),
-  Music(music_author: "Sardor", music_title: "Music name"),
-  Music(music_author: "Bunyodbek", music_title: "Music name"),
-  // Music(music_author: "Yusuf", music_title: "Music name")
-];
