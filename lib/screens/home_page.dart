9611d773e5117dfb9d0185252c976bd6a1da62f2
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-24 18:32:23 +0500

    home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,6 +5,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: const Drawer(),
+      appBar: AppBar(
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: const [
+          PlaylistCard(),
+          PlaylistCard(),
+          PlaylistCard(),
+          PlaylistCard(),
+          PlaylistCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class PlaylistCard extends StatelessWidget {
+  const PlaylistCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+      child: Column(children: [
+        Image.asset('assets/images/Group 47.png'),
+        const Text('Liked Songs'),
+      ]),
+    );
   }
 }
