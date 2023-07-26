7df2b986a9eb2a48cd74d0104af3fd40d17adb27
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-26 10:28:56 +0500

    datasource

--- lib/features/music_player/data/models/music_model.dart
+++ lib/features/music_player/data/models/music_model.dart
@@ -1,11 +1,39 @@
 class MusicModel {
-  int id;
-  String name;
+  final int? id;
+  final String? title;
+  final String? artist;
+  final String? album;
+  final String? imgPath;
+  final String? path;
 
   MusicModel({
     required this.id,
-    required this.name,
+    required this.title,
+    required this.artist,
+    required this.album,
+    required this.imgPath,
+    required this.path,
   });
+
+  factory MusicModel.fromJson(Map<String, dynamic> json) {
+    return MusicModel(
+      id: json['id'],
+      title: json['title'],
+      artist: json['artist'],
+      album: json['album'],
+      imgPath: json['imgPath'],
+      path: json['path'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'artist': artist,
+      'album': album,
+      'imgPath': imgPath,
+      'path': path,
+    };
+  }
 }
-
-
